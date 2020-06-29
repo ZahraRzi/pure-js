0905df1d2c18d482e77bc1d92641f459aec691ef
hamednn76 <hamed.navabian@gmail.com>
2020-06-30 01:02:02 +0430

    comments

--- incr-decr.js
+++ incr-decr.js
@@ -1,21 +1,20 @@
+function getId(id) {
+  return document.getElementById(id);
+}
 
-        function getId(id) {
-            return document.getElementById(id);
-        }
+let elements = {
+  number: getId("numbers"),
+  increase: getId("incr"),
+  decrease: getId("decr"),
+};
 
+elements.increase.addEventListener("click", function () {
+  // return is not necessary here, you can remove it;
+  // if you change the #numbers input to div, p or span, the below line should be change
+  // Also in decrease action
+  return elements.number.value++;
+});
 
-
-        let elements = {
-            number: getId("numbers"),
-            increase: getId("incr"),
-            decrease: getId("decr"),
-        }
-
-
-        elements.increase.addEventListener("click", function () {
-            return elements.number.value++;
-        })
-
-        elements.decrease.addEventListener("click", function () {
-            return elements.number.value--;
-        })
+elements.decrease.addEventListener("click", function () {
+  return elements.number.value--;
+});
--- listLoop.js
+++ listLoop.js
@@ -2,6 +2,8 @@ const person = ["BMW", "Benz", "Pride"];
 let BMW = `<li>${person[0]}</li>`;
 
 for (let i = 1; i < person.length; i++) {
-    BMW += `<li>${person[i]}</li>`;
-    document.getElementById("list").innerHTML = BMW;
+  BMW += `<li>${person[i]}</li>`;
+  document.getElementById("list").innerHTML = BMW; // this line should be after loop, when the list is generated and finally render it in to the DOM.
 }
+
+// document.getElementById("list").innerHTML = BMW; <=== HERE
