59d0cb0c78a406a50fed9ea3aa32b5d8b92cb733
ZahraRzi <zahra.1994rezaei@gmail.com>
2020-07-01 18:07:57 +0430

    modified!

--- listLoop.js
+++ listLoop.js
@@ -3,7 +3,6 @@ let BMW = `<li>${person[0]}</li>`;
 
 for (let i = 1; i < person.length; i++) {
   BMW += `<li>${person[i]}</li>`;
-  document.getElementById("list").innerHTML = BMW; // this line should be after loop, when the list is generated and finally render it in to the DOM.
 }
 
-// document.getElementById("list").innerHTML = BMW; <=== HERE
+document.getElementById("list").innerHTML = BMW;
