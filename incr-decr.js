0a13099a7caa46ebe27190181144cb5f03b13c0e
ZahraRzi <zahra.1994rezaei@gmail.com>
2020-07-01 18:02:56 +0430

    task modified!

--- incr-decr.js
+++ incr-decr.js
@@ -8,13 +8,16 @@ let elements = {
   decrease: getId("decr"),
 };
 
+let x = 0;
+
 elements.increase.addEventListener("click", function () {
   // return is not necessary here, you can remove it;
   // if you change the #numbers input to div, p or span, the below line should be change
   // Also in decrease action
-  return elements.number.value++;
+
+  elements.number.textContent = x++;
 });
 
 elements.decrease.addEventListener("click", function () {
-  return elements.number.value--;
+   elements.number.textContent = x--;
 });
