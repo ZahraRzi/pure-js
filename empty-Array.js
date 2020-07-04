8394e715c39bf95e6bf8bb9704a10423426328b9
ZahraRzi <zahra.1994rezaei@gmail.com>
2020-07-05 01:07:30 +0430

    empty-array.js file added

--- empty-Array.js
+++ empty-Array.js
@@ -1,17 +1,18 @@
 
-    const input = document.getElementById("cars");
-    const listItems = document.getElementById("list2");
-    const btn = document.getElementById("btn");
+const input = document.getElementById("cars");
+const listItems = document.getElementById("list2");
+const btn = document.getElementById("btn");
 
-    let toDoList = [];
-    let z = input.value;
-    toDoList.push(z);
+let toDoList = [];
+let z = input.value;
+toDoList.push(z);
+let i;
 
-    btn.addEventListener("onclick", function (event) {
-      event.preventDefault();
-      for (let i = 0; i < toDoList.length; i++) {
-        toDoList += `<li>${z}</li>`;
-      }
-    })
-    listItems.innerHTML = toDoList;
+btn.addEventListener("onclick", function (event) {
+  event.preventDefault();
+  for (let i = 0; i < toDoList.length; i++) {
+  z += `<li>${toDoList[i]}</li>`;
+  }
+   })
 
+listItems.innerHTML = z;
